f1ddaa56816ba9371f80bd4fd80ec555fefad1b9
Cương Nguyễn <cuong0993@gmail.com>
2026-02-20 10:05:13 +0700

    Clean

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -48,15 +48,6 @@ end
 
 return {
   {
-    "ramilito/kubectl.nvim",
-    -- use a release tag to download pre-built binaries
-    version = "2.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    dependencies = "saghen/blink.download",
-    opts = {},
-  },
-  {
     "mistricky/codesnap.nvim",
     cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapASCII" },
     opts = {
