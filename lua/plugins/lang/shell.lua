6a6169b10738a032c18fbe1b71a52d4d38385c11
Cương Nguyễn <cuong0993@gmail.com>
2026-02-21 18:05:34 +0700

    Clean

--- lua/plugins/lang/shell.lua
+++ lua/plugins/lang/shell.lua
@@ -2,6 +2,15 @@ return {
   {
     "mason-org/mason.nvim",
     opts = { ensure_installed = { "bash-language-server" } },
+      automatic_enable = {
+        exclude = {
+            "go",
+            "composer",
+            "php",
+            "julia",
+            "java"
+        }
+    }
   },
   {
     "neovim/nvim-lspconfig",
